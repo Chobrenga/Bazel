bd3a0c367f55f657c041c00dde4f5d96102a76f6
WeebAlert <alesage14667@gmail.com>
2019-06-07 22:33:46 -0400

    Setup basic Application and Entry Point

---
+++ Hazel/src/Hazel/Application.cpp
@@ -0,0 +1,20 @@
+#include "Application.h"
+
+namespace Hazel {
+
+	Application::Application()
+	{
+		//This is some gay shit
+	}
+
+	Application::~Application()
+	{
+		//Some gayer shit
+	}
+
+	void Application::Run()
+	{
+		while (true); // Niger
+	}
+
+}
