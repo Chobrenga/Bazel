ae2b8bd8601aa565eec9f2f2b5def5a0730b1249
WeebAlert <alesage14667@gmail.com>
2019-06-09 10:58:20 -0400

    Added a Premake build system

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -6,7 +6,11 @@ extern Hazel::Application* Hazel::CreateApplication();
 
 int main(int argc, char** argv)
 {
-	printf("Hazel Engine");
+	Hazel::Log::Init();
+	HZ_CORE_INFO("Initialized Log!");
+	int a = 5;
+	HZ_INFO("Initialized Log! a={0}",a);
+
 	auto app = Hazel::CreateApplication();
 	app->Run();
 	delete app;
